7b785d6b7ac470814309a655d98dc0798eba1eac
liz <liz@heroku.com>
2015-09-28 15:02:05 -0400

    four-space tabs

--- src/aws_client.erl
+++ src/aws_client.erl
@@ -21,115 +21,115 @@
 %%====================================================================
 
 start_link() ->
-  gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 stop() ->
-  gen_server:stop(?MODULE).
+    gen_server:stop(?MODULE).
 
 make_client() ->
-  make_client(metadata, []).
+    make_client(metadata, []).
 make_client(metadata, Opts) ->
-  gen_server:call(?MODULE, {add_creds, metadata, Opts}).
+    gen_server:call(?MODULE, {add_creds, metadata, Opts}).
 % Backwards compatibility
 make_client(AccessKey, Secret, Region) when is_binary(Region) ->
-  make_client(static, {AccessKey, Secret}, [{region, Region}]);
+    make_client(static, {AccessKey, Secret}, [{region, Region}]);
 make_client(static, {AccessKey, Secret}, Opts) when is_list(Opts) ->
-  gen_server:call(?MODULE, {add_creds, {static, AccessKey, Secret}, Opts}).
+    gen_server:call(?MODULE, {add_creds, {static, AccessKey, Secret}, Opts}).
 
 get_creds(Ref) ->
-  case ets:lookup(creds, Ref) of
-    [{_, Creds}] -> Creds;
-    [] -> undefined
-  end.
+    case ets:lookup(creds, Ref) of
+        [{_, Creds}] -> Creds;
+        [] -> undefined
+    end.
 
 %%====================================================================
 %% Behaviour
 %%====================================================================
 init(_Args) ->
-  ets:new(creds, [set, named_table]),
-  ets:new(creds_dedup, [set, named_table]),
-  {ok, #state{}}.
+    ets:new(creds, [set, named_table]),
+    ets:new(creds_dedup, [set, named_table]),
+    {ok, #state{}}.
 
 terminate(_Reason, _State) ->
-  ok.
+    ok.
 
 handle_call({add_creds, metadata, Opts}, _From, State) ->
-  case ets:lookup(creds_dedup, metadata) of
-    [{_, Ref}] -> {reply, {ok, Ref}, State};
-    [] ->
-      {ok, Role} = get_role(),
-      {ok, AccessKey, SecretKey, Expiry} = get_metadata_creds(Role),
-      {Region, Endpoint} = parse_opts(Opts),
-      Ref = make_ref(),
-      true = ets:insert(creds_dedup, {metadata, Ref}),
-      true = ets:insert(creds, {Ref, #{access_key => AccessKey,
-                                       secret_key => SecretKey,
-                                       region     => Region,
-                                       endpoint   => Endpoint}}),
-      setup_update_callback(Expiry, Ref, Role),
-      {reply, {ok, Ref}, State}
-  end;
+    case ets:lookup(creds_dedup, metadata) of
+        [{_, Ref}] -> {reply, {ok, Ref}, State};
+        [] ->
+            {ok, Role} = get_role(),
+            {ok, AccessKey, SecretKey, Expiry} = get_metadata_creds(Role),
+            {Region, Endpoint} = parse_opts(Opts),
+            Ref = make_ref(),
+            true = ets:insert(creds_dedup, {metadata, Ref}),
+            true = ets:insert(creds, {Ref, #{access_key => AccessKey,
+                                             secret_key => SecretKey,
+                                             region     => Region,
+                                             endpoint   => Endpoint}}),
+            setup_update_callback(Expiry, Ref, Role),
+            {reply, {ok, Ref}, State}
+    end;
 handle_call({add_creds, {static, AccessKey, SecretKey}, Opts}, _From, State) ->
-  case ets:lookup(creds_dedup, {static, AccessKey}) of
-    [{_, Ref}] -> {reply, {ok, Ref}, State};
-    [] ->
-      {Region, Endpoint} = parse_opts(Opts),
-      Ref = make_ref(),
-      true = ets:insert(creds_dedup, {{static, AccessKey}, Ref}),
-      true = ets:insert(creds, {Ref, #{access_key => AccessKey,
-                                       secret_key => SecretKey,
-                                       region     => Region,
-                                       endpoint   => Endpoint}}),
-      {reply, {ok, Ref}, State}
-  end;
+    case ets:lookup(creds_dedup, {static, AccessKey}) of
+        [{_, Ref}] -> {reply, {ok, Ref}, State};
+        [] ->
+            {Region, Endpoint} = parse_opts(Opts),
+            Ref = make_ref(),
+            true = ets:insert(creds_dedup, {{static, AccessKey}, Ref}),
+            true = ets:insert(creds, {Ref, #{access_key => AccessKey,
+                                             secret_key => SecretKey,
+                                             region     => Region,
+                                             endpoint   => Endpoint}}),
+            {reply, {ok, Ref}, State}
+    end;
 handle_call(Args, _From, State) ->
-  io:format("Unknown args ~p~n", [Args]),
-  {noreply, State}.
+    io:format("Unknown args ~p~n", [Args]),
+    {noreply, State}.
 
 handle_cast(_Message, State) ->
-  {noreply, State}.
+    {noreply, State}.
 
 handle_info({refresh_metadata, Ref, Role}, State) ->
-  {ok, AccessKey, SecretKey, Expiry} = get_metadata_creds(Role),
-  Creds = get_creds(Ref),
-  true = ets:insert(creds, {Ref, Creds#{access_key => AccessKey,
-                                        secret_key => SecretKey}}),
-  setup_update_callback(Expiry, Ref, Role),
-  {noreply, State};
+    {ok, AccessKey, SecretKey, Expiry} = get_metadata_creds(Role),
+    Creds = get_creds(Ref),
+    true = ets:insert(creds, {Ref, Creds#{access_key => AccessKey,
+                                          secret_key => SecretKey}}),
+    setup_update_callback(Expiry, Ref, Role),
+    {noreply, State};
 
 handle_info(_Message, State) ->
-  {noreply, State}.
+    {noreply, State}.
 
 code_change(_Prev, State, _Extra) ->
-  {ok, State}.
+    {ok, State}.
 
 %%====================================================================
 %% Helper funcs
 %%====================================================================
 parse_opts(Opts) ->
-  Region = proplists:get_value(region, Opts, <<"us-east-1">>),
-  Endpoint = proplists:get_value(endpoint, Opts, <<"amazonaws.com">>),
-  {Region, Endpoint}.
+    Region = proplists:get_value(region, Opts, <<"us-east-1">>),
+    Endpoint = proplists:get_value(endpoint, Opts, <<"amazonaws.com">>),
+    {Region, Endpoint}.
 
 
 get_role() ->
-  {ok, 200, _, ClientRef} = hackney:get(?CREDENTIAL_URL),
-  hackney:body(ClientRef).
+    {ok, 200, _, ClientRef} = hackney:get(?CREDENTIAL_URL),
+    hackney:body(ClientRef).
 
 
 get_metadata_creds(Role) ->
-  {ok, 200, _, ClientRef} = hackney:get([?CREDENTIAL_URL, Role]),
-  {ok, Body} = hackney:body(ClientRef),
-  Map = jsx:decode(Body, [return_maps]),
-  {ok, maps:get(<<"AccessKeyId">>, Map),
-       maps:get(<<"SecretAccessKey">>, Map),
-       maps:get(<<"Expiration">>, Map)}.
+    {ok, 200, _, ClientRef} = hackney:get([?CREDENTIAL_URL, Role]),
+    {ok, Body} = hackney:body(ClientRef),
+    Map = jsx:decode(Body, [return_maps]),
+    {ok, maps:get(<<"AccessKeyId">>, Map),
+     maps:get(<<"SecretAccessKey">>, Map),
+     maps:get(<<"Expiration">>, Map)}.
 
 setup_update_callback(Timestamp, Ref, Role) ->
-  AlertAt = seconds_until_timestamp(Timestamp) - ?ALERT_BEFORE_EXPIRY,
-  erlang:send_after(AlertAt, ?MODULE, {refresh_metadata, Ref, Role}).
+    AlertAt = seconds_until_timestamp(Timestamp) - ?ALERT_BEFORE_EXPIRY,
+    erlang:send_after(AlertAt, ?MODULE, {refresh_metadata, Ref, Role}).
 
 seconds_until_timestamp(Timestamp) ->
-  calendar:datetime_to_gregorian_seconds(iso8601:parse(Timestamp))
-  - (erlang:system_time(seconds)
-     + calendar:datetime_to_gregorian_seconds({{1970,1,1},{0,0,0}})).
+    calendar:datetime_to_gregorian_seconds(iso8601:parse(Timestamp))
+    - (erlang:system_time(seconds)
+       + calendar:datetime_to_gregorian_seconds({{1970,1,1},{0,0,0}})).
--- test/aws_client_SUITE.erl
+++ test/aws_client_SUITE.erl
@@ -20,24 +20,24 @@ groups() -> [{mecked_metadata, [],
                metadata_dedup]}].
 
 init_per_suite(Config) ->
-  application:load(aws),
-  {ok, Apps} = application:ensure_all_started(aws),
-  [{apps, Apps}|Config].
+    application:load(aws),
+    {ok, Apps} = application:ensure_all_started(aws),
+    [{apps, Apps}|Config].
 
 end_per_suite(Config) ->
-  Apps = ?config(apps, Config),
-  lists:foreach(fun(App) -> ok = application:stop(App) end,
-                lists:reverse(Apps)),
-  Config.
+    Apps = ?config(apps, Config),
+    lists:foreach(fun(App) -> ok = application:stop(App) end,
+                  lists:reverse(Apps)),
+    Config.
 
 init_per_group(mecked_metadata, Config) ->
-  RoleList = make_ref(),
-  Creds = make_ref(),
-  Role = <<"cedar-wallet-user">>,
-  AccessKey = <<"SomeAccessKey">>,
-  SecretKey = <<"SecretAccessKey">>,
-  Expiry = <<"2019-09-25T23:43:56Z">>,
-  Body = <<"{
+    RoleList = make_ref(),
+    Creds = make_ref(),
+    Role = <<"cedar-wallet-user">>,
+    AccessKey = <<"SomeAccessKey">>,
+    SecretKey = <<"SecretAccessKey">>,
+    Expiry = <<"2019-09-25T23:43:56Z">>,
+    Body = <<"{
       \"Code\" : \"Success\",
       \"LastUpdated\" : \"2015-09-25T17:19:52Z\",
       \"Type\" : \"AWS-HMAC\",
@@ -46,64 +46,64 @@ init_per_group(mecked_metadata, Config) ->
       \"Token\" : \"token\",
       \"Expiration\" : \"2019-09-25T23:43:56Z\"
    }">>,
-  meck:expect(hackney, get, fun(URL) when is_binary(URL) ->
-                                {ok, 200, {}, RoleList};
-                                ([URL, Rolef]) when is_binary(URL)
-                                                    andalso Role == Rolef ->
-                                {ok, 200, {}, Creds}
-                            end),
+    meck:expect(hackney, get, fun(URL) when is_binary(URL) ->
+                                      {ok, 200, {}, RoleList};
+                                 ([URL, Rolef]) when is_binary(URL)
+                                                     andalso Role == Rolef ->
+                                      {ok, 200, {}, Creds}
+                              end),
 
-  meck:expect(hackney, body, fun(Ref) when Ref == RoleList ->
-                                 {ok, Role};
-                                (Ref) when Ref == Creds ->
-                                 {ok, Body}
-                             end),
-  [{access_key, AccessKey},
-   {secret_key, SecretKey},
-   {expiry, Expiry}|Config].
+    meck:expect(hackney, body, fun(Ref) when Ref == RoleList ->
+                                       {ok, Role};
+                                  (Ref) when Ref == Creds ->
+                                       {ok, Body}
+                               end),
+    [{access_key, AccessKey},
+     {secret_key, SecretKey},
+     {expiry, Expiry}|Config].
 
 end_per_group(mecked_metadata, Config) ->
-  meck:unload(hackney),
-  Config.
+    meck:unload(hackney),
+    Config.
 
 init_per_testcase(_, Config) -> Config.
 end_per_testcase(_, Config) ->
-  ets:delete_all_objects(creds),
-  ets:delete_all_objects(creds_dedup),
-  Config.
+    ets:delete_all_objects(creds),
+    ets:delete_all_objects(creds_dedup),
+    Config.
 
 test_insert_static_creds(_Config) ->
-  AccessKey = <<"SomeAccessKey">>,
-  SecretKey = <<"SomeSecretKey">>,
-  Region = <<"eu-west-1">>,
-  {ok, Ref} = aws_client:make_client(static, {AccessKey, SecretKey},
-                                     [{region, Region}]),
+    AccessKey = <<"SomeAccessKey">>,
+    SecretKey = <<"SomeSecretKey">>,
+    Region = <<"eu-west-1">>,
+    {ok, Ref} = aws_client:make_client(static, {AccessKey, SecretKey},
+                                       [{region, Region}]),
 
-  ?assertMatch(#{region := Region,
-                 access_key := AccessKey,
-                 secret_key := SecretKey},
-               aws_client:get_creds(Ref)).
+    ?assertMatch(#{region := Region,
+                   access_key := AccessKey,
+                   secret_key := SecretKey},
+                 aws_client:get_creds(Ref)).
 
 test_dedup_acces_key(_Config) ->
-  AccessKey = <<"SomeOtherAccessKey">>,
-  SecretKey = <<"SomeOtherSecretKey">>,
-  Region = <<"us-west-1">>,
-  {ok, Ref} = aws_client:make_client(AccessKey, SecretKey, Region),
-  ?assertMatch({ok, Ref},
-               aws_client:make_client(static, {AccessKey, SecretKey},
-                                      [{region, Region}])).
+    AccessKey = <<"SomeOtherAccessKey">>,
+    SecretKey = <<"SomeOtherSecretKey">>,
+    Region = <<"us-west-1">>,
+    {ok, Ref} = aws_client:make_client(AccessKey, SecretKey, Region),
+    ?assertMatch({ok, Ref},
+                 aws_client:make_client(static, {AccessKey, SecretKey},
+                                        [{region, Region}])).
 
 fetch_metadata_creds(Config) ->
-  AccessKey = ?config(access_key, Config),
-  SecretKey = ?config(secret_key, Config),
-  Region = <<"eu-west-1">>,
-  {ok, Ref} = aws_client:make_client(metadata, [{region, Region}]),
-  ?assertMatch(#{region := Region,
-                 access_key := AccessKey,
-                 secret_key := SecretKey},
-               aws_client:get_creds(Ref)).
+    AccessKey = ?config(access_key, Config),
+    SecretKey = ?config(secret_key, Config),
+    Region = <<"eu-west-1">>,
+    {ok, Ref} = aws_client:make_client(metadata, [{region, Region}]),
+    ?assertMatch(#{region := Region,
+                   access_key := AccessKey,
+                   secret_key := SecretKey},
+                 aws_client:get_creds(Ref)).
 
 metadata_dedup(_Config) ->
-  {ok, Ref} = aws_client:make_client(),
-  ?assertMatch({ok, Ref},
-               aws_client:make_client(metadata, [])).
+    {ok, Ref} = aws_client:make_client(),
+    ?assertMatch({ok, Ref},
+                 aws_client:make_client(metadata, [])).
